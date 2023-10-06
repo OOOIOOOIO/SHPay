81c4c885167951af699a6b7969a64cc93160d7d4
OOOIOOOIO <polite159@gmail.com>
2023-10-06 13:52:59 +0900

    initial commit

---
+++ src/main/java/com/sh/shpay/ShPayApplication.java
@@ -0,0 +1,13 @@
+package com.sh.shpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh/shpay/ShPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.shpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
